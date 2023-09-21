afd161bb894f623da03776b53a87ae3cb7d19c57
romans.vavilovs <koccmo@gmail.com>
2023-09-21 13:42:31 +0300

    first create

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,13 @@
+import Solver.process
+
+import scala.io.StdIn
+
+object Main {
+  def main(args: Array[String]): Unit = Iterator
+    .continually(Option(StdIn.readLine()))
+    .takeWhile(_.nonEmpty)
+    .foreach { x =>
+      x map process foreach println
+    }
+
+}
