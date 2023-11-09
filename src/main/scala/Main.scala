d43d85bdc975eb5e979eea5d74acba7eaad38b16
romans.vavilovs <koccmo@gmail.com>
2023-11-09 19:13:40 +0300

    first version of file.txt reader and processed program, create answer.txt

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,21 @@
 import Solver.process
 
-import scala.io.StdIn
+import java.io.PrintWriter
+import scala.io.Source
 
 object Main {
-  def main(args: Array[String]): Unit = Iterator
-    .continually(Option(StdIn.readLine()))
-    .takeWhile(_.nonEmpty)
-    .foreach { x =>
-      x map process foreach println
-    }
+  def main(args: Array[String]): Unit = {
+    val source = Source.fromFile("C:/Users/Koccm/IdeaProjects/myPoker/src/main/scala/input/myInput.txt")
+    val lines = source.getLines().toList
+    source.close()
+
+    val processedData = lines.map(line => process(line))
+
+    val output = new PrintWriter("answer.txt")
+    processedData.foreach(output.println)
+    output.close()
+
+    println("Data processing and writing to answer.txt completed.")
+  }
 
 }
