a69b01599331630f08ea1826a4ad9a43db37c966
romans.vavilovs <koccmo@gmail.com>
2024-01-11 00:07:59 +0300

    ProcessResult OmahaHoldem flapMap - map to for comprehension

--- src/main/scala/com/mypoker/services/ProcessResult.scala
+++ src/main/scala/com/mypoker/services/ProcessResult.scala
@@ -19,17 +19,17 @@ object ProcessResult {
 
       def apply(line: String): String =
         line.split("\\s+").toList match {
-          case "texas-holdem" :: board :: hands => texasResult(board, hands)
+          case "texas-holdem" :: board :: hands => texasHoldemResult(board, hands)
           case "omaha-holdem" :: board :: hands => omahaHoldemResult(board, hands)
           case "five-card-draw" :: hands        => fiveCardDrawResult(hands)
-          case x :: _                           => "Unrecognized game type"
+          case gameType :: _                    => s"Unrecognized game type $gameType"
           case _                                => "Invalid input"
         }
 
       implicit val handOrdering: Ordering[Hand] =
         Ordering.by[Hand, Int](_.strength.getOrElse(0)) orElse Ordering.by[Hand, String](_.toString)
 
-      private def texasResult(board: String, hands: List[String]): String = {
+      private def texasHoldemResult(board: String, hands: List[String]): String = {
         val result: Either[ValidationError, List[Hand]] =
           validate
             .texasHoldem(board, hands)
@@ -68,14 +68,16 @@ object ProcessResult {
             .map {
               case OmahaHoldem(board, hands) =>
                 hands.map { hand =>
-                  val combinedBoard        = board.cards.combinations(3).toList
-                  val combinedHand         = hand.cards.combinations(2).toList
-                  val combinations        = combinedBoard.flatMap(boardCombination =>
-                    combinedHand.map(handCombination => boardCombination ++ handCombination)
+                  hand.copy(strength =
+                    Some(
+                      {
+                        for {
+                          combinedBoard <- board.cards.combinations(3).toList
+                          combinedHand  <- hand.cards.combinations(2).toList
+                        } yield calculateStrength(combinedBoard ++ combinedHand)
+                      }.max
+                    )
                   )
-                  val topValueCombinations = combinations.map(combination => calculateStrength(combination)).max
-
-                  hand.copy(strength = Some(topValueCombinations))
                 }.sorted
             }
 
@@ -85,5 +87,8 @@ object ProcessResult {
         }
       }
 
+//      1 (String, List[String]) => Either[ValidationError, GameType]
+//      2 (        List[String]) => Either[ValidationError, GameType]
+//      3 (String, List[String]) => Either[ValidationError, GameType]
     }
 }
